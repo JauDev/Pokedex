8864fe5da5b9ee4812f28d0d83811b9babc48e09
JaumeGandara <jaumegandara@gmail.com>
2025-04-30 20:03:30 +0200

    fix: :art: deprecation alert correction

--- test/test_repo.dart
+++ test/test_repo.dart
@@ -1,9 +1,10 @@
 import 'package:pokedex/services/pokemon_repository.dart';
+import 'package:pokedex/utils/logger.dart';
 
 void main() async {
   final repo = PokemonRepository();
   final page = await repo.fetchPage(limit: 1, offset: 0);
-  print('Primer Pokémon: ${page.results.first.name}');
+  log.i('Primer Pokémon: ${page.results.first.name}');
   final detail = await repo.fetchPokemon(page.results.first.name);
-  print('ID de ${detail.name}: ${detail.id}');
+  log.i('ID de ${detail.name}: ${detail.id}');
 }
