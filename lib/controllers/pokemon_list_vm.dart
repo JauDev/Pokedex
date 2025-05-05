922508336ea9cfe36394d7c4f69f29fdd35ec24a
JaumeGandara <jaumegandara@gmail.com>
2025-05-05 16:15:30 +0200

    feat: :sparkles: fitxer viewmodel

---
+++ lib/controllers/pokemon_list_vm.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+import '../services/pokemon_repository.dart';
+import '../models/pokemon_summary.dart';
+import '../models/pokemon_page.dart';
+
+class PokemonListVM extends ChangeNotifier {
+  
+}
