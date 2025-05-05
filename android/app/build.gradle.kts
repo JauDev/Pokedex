a35beacd716ebea462d8b9b26f6697e9e30e5a5f
JaumeGandara <jaumegandara@gmail.com>
2025-05-05 16:08:01 +0200

    chore: :arrow_up: sdk i ndk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.pokedex"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
